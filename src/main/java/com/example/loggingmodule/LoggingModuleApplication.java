d097299f1849f48f4b55eee7e7e54b752dabc157
chienbui09 <buiducchientlf2209@gmail.com>
2023-04-22 23:37:12 +0700

    init project

---
+++ src/main/java/com/example/loggingmodule/LoggingModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.loggingmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loggingmodule/LoggingModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggingmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
